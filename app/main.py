1a07120aed6236e4de5caa3c01d4cbf5ce590efc
ShubhenduADutta <shubhenduanupam@gmail.com>
2023-10-29 20:16:35 +0530

    Add create_user method (post request)

--- app/main.py
+++ app/main.py
@@ -178,3 +178,13 @@ def update_post(post_id: int, post: schemas.PostCreate,
     db.commit()
     # conn.commit()
     return post_query.first()
+
+
+@app.post("/users", status_code=status.HTTP_201_CREATED)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    new_user = models.User(**user.model_dump())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
