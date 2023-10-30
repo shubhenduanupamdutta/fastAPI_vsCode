0c7ca40f3aba2ae20b14e885bf9986b45c3e5f88
ShubhenduADutta <shubhenduanupam@gmail.com>
2023-10-30 22:04:06 +0530

    Add verify_token and get_current_user to oauth2.py

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,6 +1,11 @@
 from datetime import datetime, timedelta
-from jose import jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from app import schemas
 from .config.config import ACCESS_TOKEN_EXPIRE_MINUTES, ALGORITHM, SECRET_KEY
+
+oauth2_schema = OAuth2PasswordBearer(tokenUrl="login")
 
 
 def create_access_token(data: dict):
@@ -18,3 +23,23 @@ def create_access_token(data: dict):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+def verify_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        id = payload.get("users_id")
+        if not id:
+            raise credentials_exception
+        token_data = schemas.TokenData(id=id)
+    except JWTError:
+        raise credentials_exception
+    return token_data
+
+
+def get_current_user(token: str = Depends(oauth2_schema)):
+    credential_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"})
+    return verify_token(token, credential_exception)
