c51133b91975248529f3869f071b86e5709b410a
ShubhenduADutta <shubhenduanupam@gmail.com>
2023-10-30 22:10:26 +0530

    Change raise HTTPException to 403

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -31,7 +31,7 @@ def login(user_credentials: OAuth2PasswordRequestForm = Depends(),
         models.User.email == user_credentials.username).first()
     if (not user) or (not verify_password(user_credentials.password,
                                           user.password)):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                             detail="Invalid Credentials")
     access_token = oauth2.create_access_token(data={"user_id": user.id})
     return {"access_token": access_token, "token_type": "bearer"}
