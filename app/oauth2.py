171c5adb7784e2f18456b5db3d0d63391d09e876
ShubhenduADutta <shubhenduanupam@gmail.com>
2023-10-31 19:54:19 +0530

    remove unnecessary import

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,5 +1,4 @@
 from datetime import datetime, timedelta
-from re import A
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
