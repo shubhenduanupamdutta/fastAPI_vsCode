97d48e31d6274887cde1fbea9f3cacf0bf7c6852
ShubhenduADutta <shubhenduanupam@gmail.com>
2023-10-31 20:01:05 +0530

    Create votes table in database using model

--- app/models.py
+++ app/models.py
@@ -27,3 +27,12 @@ class User(Base):
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=func.now())
+
+
+class Vote(Base):
+    __tablename__ = "votes"
+
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"),
+                     primary_key=True, nullable=False)
+    post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"),
+                     primary_key=True, nullable=False)
