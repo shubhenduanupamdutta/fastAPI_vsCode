707d399dd366dc664efe7d67cf7d662df39bbe0b
ShubhenduADutta <shubhenduanupam@gmail.com>
2023-11-01 10:37:38 +0530

    CORS policy adjustment to allow other hosts.

--- app/main.py
+++ app/main.py
@@ -5,16 +5,27 @@ by Sanjeev Thyagarajan
 """
 
 from fastapi import FastAPI
-from .database import engine
-from . import models
+from fastapi.middleware.cors import CORSMiddleware
+# from .database import engine
+# from . import models
 from .routers import post, user, auth, vote
 
 
 # load database tables i.e. models
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
