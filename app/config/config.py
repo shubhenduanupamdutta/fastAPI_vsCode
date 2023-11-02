cb5ad734e97500fd71895886f355981435e6c16b
ShubhenduADutta <shubhenduanupam@gmail.com>
2023-11-02 10:15:26 +0530

    Add app name to config

--- app/config/config.py
+++ app/config/config.py
@@ -16,6 +16,7 @@ class Settings(BaseSettings):
     access_token_expire_minutes: int
     azure_sub_id: str
     repo_url: str
+    web_app_name: str
 
     class Config:
         env_file = ".env"
@@ -33,6 +34,7 @@ algorithm = settings.algorithm
 access_token_expire_minutes = settings.access_token_expire_minutes
 repo_url = settings.repo_url
 azure_sub_id = settings.azure_sub_id
+web_app_name = settings.web_app_name
 
 # HOST = os.environ['HOST']
 # DB_NAME = os.environ['DATABASE']
