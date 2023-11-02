824368be7e1fa976c08ae40896c33fa9c416bccb
ShubhenduADutta <shubhenduanupam@gmail.com>
2023-11-02 14:29:07 +0530

    to push and redeploy

--- startup.sh
+++ startup.sh
@@ -1,2 +1,1 @@
-alembic upgrade head;
-gunicorn app.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:80;
+alembic upgrade head; gunicorn app.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:80;
