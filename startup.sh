f5e862962f1b63dc467b47e7871caecc75bb3099
ShubhenduADutta <shubhenduanupam@gmail.com>
2023-11-02 14:20:50 +0530

    changed location of app

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 alembic upgrade head;
-gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:80;
+gunicorn app.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:80;
