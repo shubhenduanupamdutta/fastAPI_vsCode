3bfdcab4fa0f13137dbb2aeaad8daee7ebe98415
ShubhenduADutta <shubhenduanupam@gmail.com>
2023-11-02 14:58:52 +0530

    changed to uvicorn

--- startup.sh
+++ startup.sh
@@ -1,1 +1,2 @@
-alembic upgrade head; gunicorn app.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:80;
+uvicorn app.main:app --host 0.0.0.0 --port 80
+# alembic upgrade head; gunicorn app.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:80;
