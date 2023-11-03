957fceb85aa32dad179af191e6a107716491b1a7
ShubhenduADutta <shubhenduanupam@gmail.com>
2023-11-03 06:55:43 +0530

    Using gunicorn to run on production

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
-alembic upgrade head; uvicorn app.main:app --host 0.0.0.0;
-# alembic upgrade head; gunicorn app.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:80;
+alembic upgrade head;
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker app.main:app --bind 0:0:0:0:8000;
