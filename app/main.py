7918a40b4fbfcbed595ad5ea087ca1647bdeccaa
ShubhenduADutta <shubhenduanupam@gmail.com>
2023-11-08 09:11:13 +0530

    Checking Deploying Operation

--- app/main.py
+++ app/main.py
@@ -44,6 +44,8 @@ def root():
         "message": "Welcome to my API",
         "New on 04th November 2023":
             "Deployed on AWS EC2 using CI/CD using GitHub Actions.",
-        "Documentation_SwaggerUI": "https://fastapishubhendu.xyz/docs/",
-        "Documentation_Redocly": "https://fastapishubhendu.xyz/redoc/"
+        "URLs": {
+            "Documentation_SwaggerUI": "https://fastapishubhendu.xyz/docs/",
+            "Documentation_Redocly": "https://fastapishubhendu.xyz/redoc/"
+        }
     }
